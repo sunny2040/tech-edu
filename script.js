d8cf846231e00f6217927ae65f92a242cf465240
admin <sunny.kumarr010@gmail.com>
2024-07-15 14:08:50 +0530

    edu

---
+++ script.js
@@ -0,0 +1,30 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showNextSlide() {
+    slides[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000);
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+});
+
